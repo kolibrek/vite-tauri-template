8183faf4c0fe20f4ea5457ca396d0933e3ee6c26
kolibrek <kolibrek@users.noreply.github.com>
2022-01-29 22:29:56 -0700

    Removing vuetify

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,9 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
-import vuetify from '@vuetify/vite-plugin';
 
 export default defineConfig({
   plugins: [
     vue(),
-    vuetify({
-      autoImport: true
-    })
   ],
   define: {
     'process.env': {}
